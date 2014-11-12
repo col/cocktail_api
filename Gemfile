6af4b30503ab08a746700a01fb647361a10d9981
Colin Harris <col.w.harris@gmail.com>
2014-11-12 23:42:10 +0800

    Updated to include Postgres

--- Gemfile
+++ Gemfile
@@ -2,8 +2,19 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'json'
+gem 'data_mapper'
+
+group :development do
+ gem 'sqlite3'
+ gem 'dm-sqlite-adapter'
+end
 
 group :test do
   gem 'rspec'
   gem 'rack-test'
+end
+
+group :production do
+ gem 'pg'
+ gem 'dm-postgres-adapter'
 end
--- cocktail_api.rb
+++ cocktail_api.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 require 'json'
+require 'data_mapper'
+
+class Drink
+  include DataMapper::Resource
+  property :id, 	Serial
+  property :name,   String
+end
+
+configure do
+  DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{Dir.pwd}/development.sqlite3"))    
+  DataMapper.auto_upgrade!
+end
 
 get '/' do
   content_type :json  
